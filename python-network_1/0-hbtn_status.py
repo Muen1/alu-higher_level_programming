d8bf97b2377ba2b9483ac80a59b8d21ebc66a9a1
Cynthia Mutie <c.mutie@alustudent.com>
2024-03-23 16:09:04 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,8 +2,11 @@
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
+    url = 'https://alu-intranet.hbtn.io/status'
+
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
